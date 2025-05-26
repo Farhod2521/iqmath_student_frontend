f95ae85fbad472018c1f3fd452df498f0e99a36f
muhammadali0728 <solijonovmuhammadali0728@gmail.com>
2025-05-26 21:55:24 +0500

    bug fixed

--- src/pages/dashboard/student/subjects/[id]/[chapterId]/[topicId]/question/index.js
+++ src/pages/dashboard/student/subjects/[id]/[chapterId]/[topicId]/question/index.js
@@ -121,25 +121,26 @@ export default function StudentTestPage() {
         answers: q.sub_questions.map((sub) => wrapPlainMath((compositeAnswers[q.id] || {})[sub.id] || ''))
       }))
 
-    checkMyResults(
-      {
-        url: URLS.studentCheckAnswer,
-        attributes: { choice_answers, composite_answers, text_answers },
-        config: { headers: { Authorization: `Bearer ${session?.accessToken}` } }
-      },
-      {
-        onSuccess: (res) => {
-          setScore(res)
-          setResults(res)
-          setTextAnswers({})
-          setCompositeAnswers({})
-          setChoiceAnswers({})
-          setShowResult(true)
-          toast.success('Siz testni yakunladingiz!')
-        },
-        onError: () => toast.error("Testni to'liq bajaring")
-      }
-    )
+    console.log('composite_answers', composite_answers)
+    // checkMyResults(
+    //   {
+    //     url: URLS.studentCheckAnswer,
+    //     attributes: { choice_answers, composite_answers, text_answers },
+    //     config: { headers: { Authorization: `Bearer ${session?.accessToken}` } }
+    //   },
+    //   {
+    //     onSuccess: (res) => {
+    //       setScore(res)
+    //       setResults(res)
+    //       setTextAnswers({})
+    //       setCompositeAnswers({})
+    //       setChoiceAnswers({})
+    //       setShowResult(true)
+    //       toast.success('Siz testni yakunladingiz!')
+    //     },
+    //     onError: () => toast.error("Testni to'liq bajaring")
+    //   }
+    // )
   }
 
   const selectedList = useMemo(() => {
@@ -329,45 +330,47 @@ export default function StudentTestPage() {
                     ))}
                   {selectedQuestion?.question_type === 'composite' && (
                     <div className="flex flex-col gap-4">
-                      {selectedQuestion?.sub_questions?.map((item, index) => (
-                        <div key={index} className="flex items-center gap-4">
-                          <span className=" text-gray-800 text-[16px]">
+                      {selectedQuestion?.sub_questions?.map((item, index) => {
+                        const text1 = i18n.language === 'uz' ? item?.text1_uz : item?.text1_ru
+                        const text2 = i18n.language === 'uz' ? item?.text2_uz : item?.text2_ru
+                        return (
+                          <div key={index}>
                             <MathJaxContext
+                              key={index}
                               config={{
                                 loader: { load: ['input/tex', 'output/chtml'] }
                               }}
                             >
-                              <MathJax dynamic>
-                                <div> {i18n.language === 'uz' ? item?.text1_uz : item?.text1_ru}</div>
-                              </MathJax>
+                              <div className="flex items-center gap-4">
+                                <span className=" text-gray-800 text-[16px]">
+                                  <MathJax dynamic key={`${selectedQuestion.id}-${item.id}-text1`}>
+                                    <span> {text1}</span>
+                                  </MathJax>
+                                </span>
+                                <EditableMathField
+                                  latex={compositeAnswers[selectedQuestion.id]?.[item.id] || ''}
+                                  onChange={(mathField) => {
+                                    setCompositeAnswers((prev) => ({
+                                      ...prev,
+                                      [selectedQuestion.id]: {
+                                        ...(prev[selectedQuestion.id] || {}),
+                                        [item.id]: mathField.latex()
+                                      }
+                                    }))
+                                  }}
+                                  placeholder="..."
+                                  onFocus={() => setActiveInputId(item.id)}
+                                  style={compositeMathStyle}
+                                />
+
+                                <MathJax dynamic key={`${selectedQuestion.id}-${item.id}-text2`}>
+                                  <span>{text2}</span>
+                                </MathJax>
+                              </div>
                             </MathJaxContext>
-                          </span>
-                          <EditableMathField
-                            latex={compositeAnswers[selectedQuestion.id]?.[item.id] || ''}
-                            onChange={(mathField) => {
-                              setCompositeAnswers((prev) => ({
-                                ...prev,
-                                [selectedQuestion.id]: {
-                                  ...(prev[selectedQuestion.id] || {}),
-                                  [item.id]: mathField.latex()
-                                }
-                              }))
-                            }}
-                            placeholder="..."
-                            onFocus={() => setActiveInputId(item.id)}
-                            style={compositeMathStyle}
-                          />
-                          <MathJaxContext
-                            config={{
-                              loader: { load: ['input/tex', 'output/chtml'] }
-                            }}
-                          >
-                            <MathJax dynamic>
-                              <span>{i18n.language === 'uz' ? item?.text2_uz : item?.text2_ru}</span>
-                            </MathJax>
-                          </MathJaxContext>
-                        </div>
-                      ))}
+                          </div>
+                        )
+                      })}
                     </div>
                   )}
 
